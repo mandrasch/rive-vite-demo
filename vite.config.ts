165f7e0a660c05be7a386d6466ab136f501b43d1
Matthias Andrasch <matthias.andrasch@dotsandlines.io>
2024-01-04 21:18:27 +0100

    chore: add optimizeDeps settings to include @rive-app/canvas dep

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   assetsInclude: ["**/*.riv"],
+  // TODO: is this necessary / helpful?
+  optimizeDeps: {
+    include: ["@rive-app/canvas"],
+  },
 });
